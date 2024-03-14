38a7c51f09da113f8b453cd740bd69248ff0a167
HTKA0314 <124349336+HTKA0314@users.noreply.github.com>
2024-03-14 07:36:55 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,20 @@
-public class MP3 {
+class MP3 extends Item {
+    private String duration;
+
+    // Phương thức khởi tạo với đầy đủ thuộc tính
+    public MP3(String name, String description, String ID, float price, String duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+    @Override
+    void showInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+        System.out.println("Duration: " + duration);
+    }
 }
+
+
+  
