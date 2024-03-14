2be29ccde82f1e926db818240a422d208457f45d
HTKA0314 <124349336+HTKA0314@users.noreply.github.com>
2024-03-14 07:58:55 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("Song Name", "Description of the song", "111", 55.5f, "3:45");
+        System.out.println("Information of MP3:");
         mp3.showInfo();
 
-        Book book;
+        System.out.println();
+        Book book = new Book("Book Name", "Description of the book", "112", 45.3f, "Author Name", 300, "Fiction");
+        System.out.println("Information of Book:");
         book.showInfo();
-
     }
 }
